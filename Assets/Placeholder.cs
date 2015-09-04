b15bd0eebbaf5a3e4f2390b0d497cec4da846dea
moe-ped <m.berning@web.de>
2015-09-04 17:25:53 +0200

    Hallöööööö

--- Assets/Placeholder.cs
+++ Assets/Placeholder.cs
@@ -1,15 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Placeholder : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class Placeholder : MonoBehaviour 
+{
+    // Hallöööööööööö
 }
